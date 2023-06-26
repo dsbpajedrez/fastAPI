2c56fff708bd01ee0e48f50284641c699004e828
David <dsbpim@gmail.com>
2023-06-26 11:20:31 -0500

    deleting |

--- schemas/movie.py
+++ schemas/movie.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel, Field
-from typing import Optional
+from typing import Optional, Union
 
 class Movie(BaseModel):
-    id: Optional[int] | None = None
+    id: Union[Optional[int], None] = None
     title: str = Field(min_length=5, max_length=50)
     overview: str = Field(min_length=5, max_length=50)
     year: int = Field(le= 2023)
