9f45753262b343e41d3570f0a3320645609eec3f
David <dsbpim@gmail.com>
2023-06-26 11:12:41 -0500

    deploying

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import os
+import uvicorn
+
 from fastapi import FastAPI
 from config.database import session, engine, Base
 from middlewares.error_handler import ErrorHandler
 from routers.movie import movie_router
 from routers.user import user_router
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0",
+                port=int(os.environ.get("PORT", 8000)))
 
 app = FastAPI()
 
